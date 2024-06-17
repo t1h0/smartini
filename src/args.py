eb3269535cf6a304b532decd0284a36d346cf10b
Tilman Hornung (t1h0) <64684735+t1h0@users.noreply.github.com>
2024-06-17 18:28:56 +0200

    Restructure

--- src/args.py
+++ src/args.py
@@ -3,38 +3,6 @@ import re
 
 
 class Parameters:
-    """Ini parameters for reading and writing.
-
-    Args:
-        entity_delimiter (str | re.Pattern, optional): Delimiter that delimits ini
-            entities (section name, option, comment). Defaults to re.Pattern("\n").
-        comment_prefixes (str | re.Pattern | tuple[str | re.Pattern, ...], optional):
-            Prefix characters that denote a comment. If multiple are given,
-            the first will be taken for writing. Defaults to ";".
-        option_delimiters (str | re.Pattern | tuple[str | re.Pattern, ...], optional):
-            Delimiter characters that delimit keys from values. If multiple are
-            given, the first will be taken for writing. Defaults to "=".
-        continuation_allowed (bool, optional): Whether continuation of options
-            (i.e. multiline options) are allowed. Defaults to True.
-        continuation_prefix (str | re.Pattern, optional): Prefix to denote options'
-            value continuations. Defaults to re.Pattern("\t") (TAB character).
-        continuation_ignore (tuple["section" | "option" | "comment", ...], optional):
-            Entities to ignore while continuing an option's value.
-            I.e. will interpret detected entities as continuation of the
-            preceeding option's value if continuation rules are met (see other
-            continuation arguments). Defaults to empty tuple (:= interpret all
-            detected entities as new entities).
-        ignore_whitespace_lines (bool, optional): Whether to interpret lines with
-            only whitespace characters (space or tab) as empty lines.
-            Defaults to True.
-        read_undefined (bool | "section" | "option", optional):
-            Whether undefined content should be read and stored. If true, will read
-            every undefined content. If type[Section], will read undefined sections
-            and their content but not undefined options within defined sections.
-            type[Option] will read undefinied options within defined sections but
-            not undefined sections and their content. If False, will ignore
-            undefined content. Defaults to False.
-    """
 
     def __init__(
         self,
@@ -50,6 +18,38 @@ class Parameters:
         ignore_whitespace_lines: bool = True,
         read_undefined: bool | Literal["section", "option"] = False,
     ) -> None:
+        """Ini parameters for reading and writing.
+
+        Args:
+            entity_delimiter (str | re.Pattern, optional): Delimiter that delimits ini
+                entities (section name, option, comment). Defaults to re.Pattern("\n").
+            comment_prefixes (str | re.Pattern | tuple[str | re.Pattern, ...], optional):
+                Prefix characters that denote a comment. If multiple are given,
+                the first will be taken for writing. Defaults to ";".
+            option_delimiters (str | re.Pattern | tuple[str | re.Pattern, ...], optional):
+                Delimiter characters that delimit keys from values. If multiple are
+                given, the first will be taken for writing. Defaults to "=".
+            continuation_allowed (bool, optional): Whether continuation of options
+                (i.e. multiline options) are allowed. Defaults to True.
+            continuation_prefix (str | re.Pattern, optional): Prefix to denote options'
+                value continuations. Defaults to re.Pattern("\t") (TAB character).
+            continuation_ignore (tuple["section" | "option" | "comment", ...], optional):
+                Entities to ignore while continuing an option's value.
+                I.e. will interpret detected entities as continuation of the
+                preceeding option's value if continuation rules are met (see other
+                continuation arguments). Defaults to empty tuple (:= interpret all
+                detected entities as new entities).
+            ignore_whitespace_lines (bool, optional): Whether to interpret lines with
+                only whitespace characters (space or tab) as empty lines.
+                Defaults to True.
+            read_undefined (bool | "section" | "option", optional):
+                Whether undefined content should be read and stored. If true, will read
+                every undefined content. If type[Section], will read undefined sections
+                and their content but not undefined options within defined sections.
+                type[Option] will read undefinied options within defined sections but
+                not undefined sections and their content. If False, will ignore
+                undefined content. Defaults to False.
+        """
 
         self.entity_delimiter = entity_delimiter
         self.comment_prefixes = comment_prefixes
@@ -75,24 +75,24 @@ class Parameters:
 
     def validate_parameters(self, *params) -> None:
         """Validate parameters for correct functionality."""
+
         if not params or "entity_delimiter" in params:
             self.entity_delimiter = (
                 self.entity_delimiter.pattern
                 if isinstance(self.entity_delimiter, re.Pattern)
                 else re.escape(self.entity_delimiter)
             )
-        if not params or "comment_prefixes" in params:
-            self.comment_prefixes = (
-                self.comment_prefixes
-                if isinstance(self.comment_prefixes, tuple)
-                else (self.comment_prefixes,)
-            )
-        if not params or "option_delimiters" in params:
-            self.option_delimiters = (
-                self.option_delimiters
-                if isinstance(self.option_delimiters, tuple)
-                else (self.option_delimiters,)
-            )
+
+        if (not params or "comment_prefixes" in params) and not isinstance(
+            self.comment_prefixes, tuple
+        ):
+            self.comment_prefixes = (self.comment_prefixes,)
+
+        if (not params or "option_delimiters" in params) and not isinstance(
+            self.option_delimiters, tuple
+        ):
+            self.option_delimiters = (self.option_delimiters,)
+
         if not params or "continuation_prefix" in params:
             self.continuation_prefix = (
                 self.continuation_prefix.pattern
