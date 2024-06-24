e4358f7367a3041af989cef623f3d1f81cd8f9dd
Tilman Hornung (t1h0) <64684735+t1h0@users.noreply.github.com>
2024-06-24 18:08:16 +0200

    Move init to respective package folder

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from src.smartini.interface import Schema, Section
-from src.smartini.args import Parameters
-from src.smartini.slots import SlotAccess, SlotDeciderMethods
-from src.smartini.entities import Comment, Option, UndefinedOption, SectionName
---
+++ src/smartini/__init__.py
@@ -0,0 +1,4 @@
+from .interface import Schema, Section
+from .args import Parameters
+from .slots import SlotAccess, SlotDeciderMethods
+from .entities import Comment, Option, UndefinedOption, SectionName
